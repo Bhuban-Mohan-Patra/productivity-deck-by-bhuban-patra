921349f4bad8ca8c472a54c9523987e4c57dd698
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-04-10 19:19:40 +0530

    Designed ui for kanban mode

--- src/components/KanbanMode/index.jsx
+++ src/components/KanbanMode/index.jsx
@@ -3,11 +3,18 @@ import React from "react";
 import { Sidebar } from "components/commons";
 import Header from "components/commons/Header";
 
+import Column from "./Column";
+
 const KanbanMode = () => (
   <div className="flex h-screen ">
     <Sidebar />
-    <div className="w-full bg-red-200 ">
+    <div className="w-full">
       <Header title="Kanban mode" />
+      <div className="mx-4 mt-20 flex h-3/4 justify-center gap-8 px-4">
+        <Column title="Todo" />
+        <Column title="In proress" />
+        <Column title="Done" />
+      </div>
     </div>
   </div>
 );
