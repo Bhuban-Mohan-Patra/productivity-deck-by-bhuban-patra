93b118ab0bcb7958a61bf56600c6fcec1d3bbd5d
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-04-10 17:12:34 +0530

    Designed ui of pomodoro mode

--- src/components/PomodoroMode/index.jsx
+++ src/components/PomodoroMode/index.jsx
@@ -8,8 +8,14 @@ const PomodoroMode = () => (
     <Sidebar />
     <div className="w-full">
       <Header title="Pomodoro mode" />
+      <div className="mt-4 flex h-4/5 items-center justify-center">
+        <div className="flex flex-col items-center rounded-md border border-black">
+          <div className="flex justify-center space-x-4">Pomodoro</div>
+          <div>timer</div>
+          <div className="flex gap-1">start</div>
+        </div>
+      </div>
     </div>
   </div>
 );
-
 export default PomodoroMode;
