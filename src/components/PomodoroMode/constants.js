8e202d2fcc66b072b93f66484cabdd4de574810b
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-04-10 18:30:01 +0530

    Added session types in pomodoro

---
+++ src/components/PomodoroMode/constants.js
@@ -0,0 +1,19 @@
+export const SESSIONS = {
+  Pomodoro: {
+    duration: 25 * 60,
+    bg: "bg-red-500",
+    // message: "Time to focus!",
+  },
+  ShortBreak: {
+    duration: 5 * 60,
+    bg: "bg-green-500",
+    // message: "Time for a short break!",
+  },
+  LongBreak: {
+    duration: 15 * 60,
+    bg: "bg-blue-500",
+    // message: "Relax, it’s a long break!",
+  },
+};
+
+export const SESSIONS_ORDER = ["Pomodoro", "ShortBreak", "LongBreak"];
