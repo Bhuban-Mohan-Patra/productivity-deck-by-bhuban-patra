b63479c18ae909bfcc0644d8ed68f6dd81d92a2d
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-04-10 12:01:16 +0530

    Added componets for routing

---
+++ src/components/NewsMode/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NewsMode = () => <div>newsmode</div>;
+
+export default NewsMode;
---
+++ src/components/PomodoroMode/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PomodoroMode = () => <div>pomodoro mode</div>;
+
+export default PomodoroMode;
