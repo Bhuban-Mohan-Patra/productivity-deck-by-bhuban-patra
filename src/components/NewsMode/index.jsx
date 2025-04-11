2aa62f61a12aad299e84a1f5897ba761f81208ba
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-04-11 15:58:59 +0530

    Added axios to news mode

--- src/components/NewsMode/index.jsx
+++ src/components/NewsMode/index.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import newsApi from "apis/news";
 import { Sidebar } from "components/commons";
 import Header from "components/commons/Header";
 
-const NewsMode = () => (
-  <div className="flex h-screen ">
-    <Sidebar />
-    <div className="w-full">
-      <Header title="News mode" />
+const NewsMode = () => {
+  const [news, setNews] = useState([]);
+
+  const fetchNews = async () => {
+    const response = await newsApi.show();
+    setNews(response.articles);
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  return (
+    <div className="flex h-screen ">
+      <Sidebar />
+      <div className="w-full">
+        <Header title="News mode" />
+        <div>
+          {news.map(newsItem => (
+            <li key={newsItem.source.id}>{newsItem.title}</li>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default NewsMode;
