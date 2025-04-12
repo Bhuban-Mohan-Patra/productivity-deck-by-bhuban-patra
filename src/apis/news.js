a2fa0790503e0989530737faa55c83081f42bbc8
Bhuban Mohan Patra <sandbox.bmp@gmail.com>
2025-04-12 13:24:00 +0530

    Fixed api call news mode

--- src/apis/news.js
+++ src/apis/news.js
@@ -1,6 +1,25 @@
+// import axios from "axios";
+
+// const fetch = ({ searchTerm }) =>
+//   axios.get("/everything", { params: { q: searchTerm } });
+
+// const newsApi = { fetch };
+
+// export default newsApi;
+
 import axios from "axios";
 
-const fetch = ({ searchTerm }) => axios.get("/", { params: { q: searchTerm } });
+// const searchArticles = ({ q }) =>
+//   axios.get("", {
+//     params: { q },
+//   });
+const fetch = ({ searchTerm }) => {
+  console.log("API calling with search term:", searchTerm);
+
+  return axios.get("/everything", {
+    params: { q: searchTerm },
+  });
+};
 
 const newsApi = { fetch };
 
